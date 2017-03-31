542413024f5747c437ebc765a72feb878933700e
Dan Lavine <dlavine@us.ibm.com>
2017-03-31 11:55:58 +0000

    Able to deploy Kafka for OpenWhisk in Kuberentes
    * cleanup Consul key values for invoker hosts
    * properly seed kafka with correct invoker topics
    * dynamically use correct kafka pod names

--- kubernetes/configure/cleanup.sh
+++ kubernetes/configure/cleanup.sh
@@ -10,6 +10,7 @@ kubectl -n openwhisk delete job configure-openwhisk
 # delete deployments
 kubectl -n openwhisk delete deployment couchdb
 kubectl -n openwhisk delete deployment consul
+kubectl -n openwhisk delete deployment kafka
 
 # delete configmaps
 kubectl -n openwhisk delete cm consul
@@ -17,3 +18,4 @@ kubectl -n openwhisk delete cm consul
 # delete services
 kubectl -n openwhisk delete service couchdb
 kubectl -n openwhisk delete service consul
+kubectl -n openwhisk delete service kafka
