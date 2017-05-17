3bfe674a015deb658ac75c7e527a04096386c912
Dan Lavine <dlavine@us.ibm.com>
2017-05-17 12:49:31 -0500

    Able to fully deploy OpenWhisk on Kube (#31)
    * Deploy OpenWhisk Nginx to Kube
    
    * Build custom nginx image that include wsk cli and blackbox
    * Can only deploy with nginx certs for 'localhost'
    
    * Able to deploy routemgmt to Kube
    
    * Add extra configurations for zookeeper and kafka.
    Able to deploy kafka properly
    
    * Add troubleshooting section for Kafka
    
    * Update deployment for controller to mach new master configurations
    
    * give more time for pods to deploy
    
    * set docker0 to promiscuous mode in travis
    
    * Print logs for configure pod in ci when it passes or fails.
    
    * wait until nginx is up and running
    
    * update timeout for travis and remove extra comments
    
    * Create secrets for kube users after deployment
    
    * Update nginx image to have proper wsk location
    
    * Update CI to perform wsk actions
    
    * use sudo when setting ip link and Sleep before performing action
    
    * Update pull policy to always pull images
    
    * Fixup travis CI timeouts
    
    * Update README

--- kubernetes/configure/cleanup.sh
+++ kubernetes/configure/cleanup.sh
@@ -14,9 +14,12 @@ kubectl -n openwhisk delete deployment zookeeper
 kubectl -n openwhisk delete deployment kafka
 kubectl -n openwhisk delete deployment controller
 kubectl -n openwhisk delete statefulsets invoker
+kubectl -n openwhisk delete deployment nginx
 
 # delete configmaps
 kubectl -n openwhisk delete cm consul
+kubectl -n openwhisk delete cm controller
+kubectl -n openwhisk delete cm nginx
 
 # delete services
 kubectl -n openwhisk delete service couchdb
@@ -25,3 +28,7 @@ kubectl -n openwhisk delete service zookeeper
 kubectl -n openwhisk delete service kafka
 kubectl -n openwhisk delete service controller
 kubectl -n openwhisk delete service invoker
+kubectl -n openwhisk delete service nginx
+
+# delete secrets
+kubectl -n openwhisk delete secret openwhisk-auth-tokens
