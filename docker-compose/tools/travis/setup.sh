26ed8c11735a612962b483b67cbd8776a939b0a0
Matt Rutkowski <mrutkows@us.ibm.com>
2017-07-06 18:07:22 -0500

    attempt to resolve path errors on Travis build.

--- docker-compose/tools/travis/setup.sh
+++ docker-compose/tools/travis/setup.sh
@@ -2,6 +2,9 @@
 set -x
 set -e
 
+DOCKER-COMPOSE='docker-compose'
+DOCKER-COMPOSE-TMP='$DOCKER-COMPOSE.tmp'
+
 # This script assumes Docker is already installed
 # see tools/travis/setup.sh
 version_exists=`(docker-compose --version | grep ${DOCKER_COMPOSE_VERSION}) || echo "false"`
@@ -9,8 +12,6 @@ echo "version exists:" version_exists
 
 if [ "${version_exists}" == "false" ]
 then
-    DOCKER-COMPOSE=docker-compose
-    DOCKER-COMPOSE-TMP=$DOCKER-COMPOSE.tmp
     echo "Installing Docker Compose ${DOCKER_COMPOSE_VERSION}"
     if [ -f $DOCKER-COMPOSE ]; then
         sudo rm /usr/local/bin/$DOCKER-COMPOSE
