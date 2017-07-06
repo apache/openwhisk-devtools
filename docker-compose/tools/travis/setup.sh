658ac423bf95fe86ae3bbc6b38612452190de96c
Matt Rutkowski <mrutkows@us.ibm.com>
2017-07-06 18:23:58 -0500

    attempt to resolve path errors on Travis build.

--- docker-compose/tools/travis/setup.sh
+++ docker-compose/tools/travis/setup.sh
@@ -2,8 +2,8 @@
 set -x
 set -e
 
-DOCKER-COMPOSE="docker-compose"
-DOCKER-COMPOSE-TMP="$DOCKER-COMPOSE.tmp"
+DOCKER_COMPOSE="docker-compose"
+DOCKER_COMPOSE_TMP="$DOCKER_COMPOSE.tmp"
 
 # This script assumes Docker is already installed
 # see tools/travis/setup.sh
@@ -13,11 +13,11 @@ echo "version exists:" version_exists
 if [ "${version_exists}" == "false" ]
 then
     echo "Installing Docker Compose ${DOCKER_COMPOSE_VERSION}"
-    if [ -f $DOCKER-COMPOSE ]; then
-        sudo rm /usr/local/bin/$DOCKER-COMPOSE
+    if [ -f /usr/local/bin/$DOCKER_COMPOSE ]; then
+        sudo rm /usr/local/bin/$DOCKER_COMPOSE
     fi
-    curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > $DOCKER-COMPOSE-TMP
-    chmod +x $DOCKER-COMPOSE-TMP
-    sudo mv $DOCKER-COMPOSE-TMP /usr/local/bin/$DOCKER-COMPOSE
+    curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > $DOCKER_COMPOSE_TMP
+    chmod +x $DOCKER_COMPOSE_TMP
+    sudo mv $DOCKER_COMPOSE_TMP /usr/local/bin/$DOCKER_COMPOSE
 fi
 echo "Docker Compose Version:" `docker-compose --version`
