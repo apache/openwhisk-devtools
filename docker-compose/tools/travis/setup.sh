71e2f7fa5a5202a4801e67dad44930d46639674a
Matt Rutkowski <mrutkows@us.ibm.com>
2017-07-06 17:53:14 -0500

    attempt to resolve path errors on Travis build.

--- docker-compose/tools/travis/setup.sh
+++ docker-compose/tools/travis/setup.sh
@@ -12,9 +12,9 @@ then
     pwd
     ls -al
     echo "Installing Docker Compose ${DOCKER_COMPOSE_VERSION}"
-    sudo rm /usr/local/bin/docker-compose
-    curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
-    chmod +x docker-compose
-    sudo mv docker-compose /usr/local/bin
+    sudo rm /usr/local/bin/docker-compose.bin
+    curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose.bin
+    chmod +x docker-compose.bin
+    sudo mv docker-compose.bin /usr/local/bin/docker-compose
 fi
 echo "Docker Compose Version:" `docker-compose --version`
