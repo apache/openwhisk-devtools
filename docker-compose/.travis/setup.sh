061f2f1054ae48d8ff8c25454f5b43bb0326d435
Matt Rutkowski <mrutkows@us.ibm.com>
2017-07-06 23:13:16 -0500

    Update setup.sh to test for existing binary.

--- docker-compose/.travis/setup.sh
+++ docker-compose/.travis/setup.sh
@@ -1,14 +1,22 @@
-# This script assumes Docker is already installed
-# see tools/travis/setup.sh
 #!/bin/bash
+set -x -e
+uname -sm
+
+DOCKER_COMPOSE="docker-compose"
+DOCKER_COMPOSE_TMP="$DOCKER_COMPOSE.bin"
 
 version_exists=`(docker-compose --version | grep ${DOCKER_COMPOSE_VERSION}) || echo "false"`
+
+# This script assumes Docker is already installed
+# Trusty for Travis SHOULD include latest docker compose (e.g., 1.13.0)
 if [ "${version_exists}" == "false" ]
 then
     echo "Installing Docker Compose ${DOCKER_COMPOSE_VERSION}"
-    sudo rm /usr/local/bin/docker-compose
-    curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose.bin
-    chmod +x docker-compose.bin
-    sudo mv docker-compose.bin /usr/local/bin/docker-compose
+    if [ -f /usr/local/bin/$DOCKER_COMPOSE ]; then
+        sudo rm /usr/local/bin/$DOCKER_COMPOSE
+    fi
+    curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > $DOCKER_COMPOSE_TMP
+    chmod +x $DOCKER_COMPOSE_TMP
+    sudo mv $DOCKER_COMPOSE_TMP /usr/local/bin/$DOCKER_COMPOSE
 fi
 echo "Docker Compose Version:" `docker-compose --version`
