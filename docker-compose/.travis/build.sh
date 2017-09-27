20cdbd1e4706e36780b38b5715b03b87feef0716
Dragos Dascalita Haut <ddragosd@gmail.com>
2017-09-26 21:06:46 -0700

    travis: made build verbose

--- docker-compose/.travis/build.sh
+++ docker-compose/.travis/build.sh
@@ -4,4 +4,4 @@ SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 ROOTDIR="$SCRIPTDIR/../"
 
 cd $ROOTDIR
-PATH=$PATH:/usr/local/bin/ make quick-start stop
+PATH=$PATH:/usr/local/bin/ VERBOSE=true make quick-start stop
