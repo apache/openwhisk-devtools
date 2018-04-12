42e0513fcf08bd2012ba62a64bc5f1a4c5fd5261
Dan Klco <daniel.klco@gmail.com>
2018-04-12 11:56:43 -0400

    Updating the travis builds to trigger the required parent build

--- java-maven/annotations/.travis/setup.sh
+++ java-maven/annotations/.travis/setup.sh
@@ -5,6 +5,13 @@ uname -sm
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 TOOLDIR="$SCRIPTDIR/../"
 
+# Build the parent so this compiles
+mkdir -p $TOOLDIR/target
+cd $TOOLDIR/target
+git clone https://github.com/klcodanr/incubator-openwhisk-devtools.git
+cd $TOOLDIR/target/incubator-openwhisk-devtools/java-maven/parent
+mvn -DskipTests=true -Dmaven.javadoc.skip=true -B -V clean install
+
 cd $TOOLDIR
 
 mvn -DskipTests=true -Dmaven.javadoc.skip=true -B -V clean install
--- java-maven/update-maven-plugin/.travis/setup.sh
+++ java-maven/update-maven-plugin/.travis/setup.sh
@@ -5,6 +5,15 @@ uname -sm
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 TOOLDIR="$SCRIPTDIR/../"
 
+# Build the parent so this compiles
+mkdir -p $TOOLDIR/target
+cd $TOOLDIR/target
+git clone https://github.com/klcodanr/incubator-openwhisk-devtools.git
+cd $TOOLDIR/target/incubator-openwhisk-devtools/java-maven/parent
+mvn -DskipTests=true -Dmaven.javadoc.skip=true -B -V clean install
+cd $TOOLDIR/target/incubator-openwhisk-devtools/java-maven/annotations
+mvn -DskipTests=true -Dmaven.javadoc.skip=true -B -V clean install
+
 cd $TOOLDIR
 
 mvn -DskipTests=true -Dmaven.javadoc.skip=true -B -V clean install
